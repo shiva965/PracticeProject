00851034e37e82f92f0c9ed99355f2f5b3032b5f
shiva965 <117453902+shiva965@users.noreply.github.com>
2022-11-21 15:21:21 +0530

    userinfo

--- WebTimeSheetManagement.Models/UserInfo.cs
+++ WebTimeSheetManagement.Models/UserInfo.cs
@@ -11,19 +11,21 @@ namespace WebTimeSheetManagement.Models
     [Table("UserInfo")]
     public class UserInfo
     {
-        // [Key]
-        //public int UID { get; set; }
+       
+        /// </summary>
+        [Key]
+        public int UID { get; set; }
 
-        //[Required(ErrorMessage = "Enter FName")]
-        //public string FName { get; set; }
+        [Required(ErrorMessage = "Enter FName")]
+        public string FName { get; set; }
 
-        //[Required(ErrorMessage = "Enter LName")]
-        //public string MName { get; set; }
+        [Required(ErrorMessage = "Enter LName")]
+        public string MName { get; set; }
 
-        //[Required(ErrorMessage = "Enter MName")]
-        //public string LName { get; set; }
+        [Required(ErrorMessage = "Enter MName")]
+        public string LName { get; set; }
 
-        //[Required(ErrorMessage = "Enter Name")]
-        //public string UName { get; set; }
+        [Required(ErrorMessage = "Enter Name")]
+        public string UName { get; set; }
     }
 }
